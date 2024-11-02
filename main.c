8a67e8dc864e7019b9da54fa641502698583695d
XuShuo <xushuo@xiaohongshu.net>
2024-11-02 17:53:53 +0800

    initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef long long int number;
+
+void print_usage_then_exit(char*);
+void panic_then_exit(char*);
+
+int main(int argc, char** argv) {
+    print_usage_then_exit(argv[0]);
+    return 0;
+}
+
+void print_usage_then_exit(char* argv1) {
+    printf("usage: %s num\n", argv1);
+    exit(1);
+}
+
+int positive(number num, int base, char* buffer) {
+
+    for(int i =0; i< )
+    return 0;
+}
+
+char* calculate_original_code(number num) {
+
+}
